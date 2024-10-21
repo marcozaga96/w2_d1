5124bf3b27f4e5af5672e4e8372e7262734a6935
Marco Zagaria <marcozagaria@live.com>
2024-10-21 14:15:10 +0200

    inizio

---
+++ u2_w1_d1/src/main/java/marcozagaria/u2_w1_d1/U2W1D1Application.java
@@ -0,0 +1,13 @@
+package marcozagaria.u2_w1_d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1D1Application.class, args);
+	}
+
+}
---
+++ u2_w1_d1/src/test/java/marcozagaria/u2_w1_d1/U2W1D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package marcozagaria.u2_w1_d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
